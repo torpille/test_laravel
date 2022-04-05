c65e6669684e45e2b2d6867025e324ad1e3d1e3f
Lischen <torpille113@gmail.com>
2022-04-05 08:19:48 +0300

    lesson 27. manual seed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->boolean('is_admin')->default(false);
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,58 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name'=>'Personal',
+            'slug'=>'personal'
+        ]);
+
+        $work = Category::create([
+            'name'=>'Work',
+            'slug'=>'work'
+        ]);
+
+        $hobbies = Category::create([
+            'name'=>'Hobbies',
+            'slug'=>'hobbies'
+        ]);
+        Post::create([
+            'title'=>'My First Post',
+            'excerpt'=>'My first post here',
+            'body'=>'This is my firs post here, lets check it',
+            'slug'=>'my-first-post',
+            'user_id'=>$user->id,
+            'category_id'=>$personal->id]);
+
+        Post::create([
+            'title'=>'My Family Post',
+            'excerpt'=>'My family post here',
+            'body'=>'This is my family post here, lets check it',
+            'slug'=>'my-family-post',
+            'user_id'=>$user->id,
+            'category_id'=>$personal->id]);
+
+        Post::create([
+            'title'=>'My Work Post',
+            'excerpt'=>'My work post here',
+            'body'=>'This is my work post here, lets check it',
+            'slug'=>'my-work-post',
+            'user_id'=>$user->id,
+            'category_id'=>$work->id]);
+
+        Post::create([
+            'title'=>'My Hobby Post',
+            'excerpt'=>'My hobby post here',
+            'body'=>'This is my hobby post here, lets check it',
+            'slug'=>'my-hobby-post',
+            'user_id'=>$user->id,
+            'category_id'=>$hobbies->id]);
+
         // \App\Models\User::factory(10)->create();
     }
 }
