cc88be6f75e33118015ae5e83afe054bae679c27
Lischen <torpille113@gmail.com>
2022-04-06 11:58:10 +0300

    lesson 31. split html to components

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,21 @@
 @extends('layout')
+
     @section('content')
-        @foreach ($posts as $post)
-        <article>
-            <a href="/posts/{{ $post->slug}} ">
-                <h1> {{$post->title}} </h1>
-            </a>
-            {{ $post->excerpt }}
-            <p>By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}"> {{$post->category->name}}</a></p>
+        @include('_posts-header')
 
-        </article>
+<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+    @yield("content")
+    <x-main-post-card/>
 
-        @endforeach
+    <div class="lg:grid lg:grid-cols-2">
+        <x-post-card/>
+        <x-post-card/>
+    </div>
+
+    <div class="lg:grid lg:grid-cols-3">
+        <x-post-card/>
+        <x-post-card/>
+        <x-post-card/>
+    </div>
+</main>
     @endsection
