6762fb797d4d06edd77527a5390f0f91351853dd
Lischen <torpille113@gmail.com>
2022-04-06 17:21:19 +0300

    lesson 32. dynamic components and css grids

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,18 +4,8 @@
         @include('_posts-header')
 
 <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-    @yield("content")
-    <x-main-post-card/>
+    <x-posts-grid :posts="$posts"/>
 
-    <div class="lg:grid lg:grid-cols-2">
-        <x-post-card/>
-        <x-post-card/>
-    </div>
-
-    <div class="lg:grid lg:grid-cols-3">
-        <x-post-card/>
-        <x-post-card/>
-        <x-post-card/>
-    </div>
 </main>
+
     @endsection
