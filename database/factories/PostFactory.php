9abe99728c4eeaa25b4c516e7139e2953d0abcec
Lischen <torpille113@gmail.com>
2022-04-07 10:46:08 +0300

    lesson 36. enlarge fake data in factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -18,8 +18,8 @@ class PostFactory extends Factory
         return [
             'title'=>$this->faker->sentence,
             'slug'=>$this->faker->slug,
-            'excerpt'=>$this->faker->sentence,
-            'body'=>$this->faker->paragraph,
+            'excerpt'=>$this->faker->paragraph(5),
+            'body'=>$this->faker->paragraph(20),
             'user_id'=>User::factory(),
             'category_id'=>Category::factory()
         ];
