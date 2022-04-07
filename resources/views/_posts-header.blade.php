e5684fa25ff3a1ea7788371907113dcd14785643
Lischen <torpille113@gmail.com>
2022-04-07 16:48:39 +0300

    lesson 37. search

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -50,7 +50,8 @@
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
             <form method="GET" action="#">
-                <input type="text" name="search" placeholder="Find something"
+                <input type="text" name="search"
+                       placeholder="{{request('search') ? request('search'): 'Find something'}}"
                        class="bg-transparent placeholder-black font-semibold text-sm">
             </form>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $posts = Post::latest('created_at');
+    if (request('search')) {
+        $posts
+            ->where('title', 'like', '%' . request('search') . '%')
+            ->orWhere('body', 'like', '%' . request('search') . '%');
+    }
     return view('posts', [
-        'posts' =>  Post::latest('created_at')->get(),
+        'posts' =>  $posts->get(),
         'categories'=> Category::all()]);
 })->name('home');
 
