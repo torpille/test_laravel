885ccfeb93b9d51d96f4182e0d9b48c8ebe5379a
Lischen <torpille113@gmail.com>
2022-04-09 22:40:35 +0300

    lesson 41. filter by author

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -28,6 +28,10 @@ class Post extends Model
         $query->when($filters['category'] ?? false, fn($query, $category) =>
             $query->whereHas('category', fn($query) => $query->where('slug', $category))
         );
+
+        $query->when($filters['author'] ?? false, fn($query, $author) =>
+        $query->whereHas('author', fn($query) => $query->where('username', $author))
+        );
     }
 
 
