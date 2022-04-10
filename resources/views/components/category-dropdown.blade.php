caefa55cf390160ae3a0efe547988e40f245af7d
Lischen <torpille113@gmail.com>
2022-04-10 19:22:53 +0300

    lesson 42. combine filter and category

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -7,7 +7,7 @@
 </x-slot>
 <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
 @foreach($categories as $category)
-    <x-dropdown-item href="/?category={{$category->slug}}"
+    <x-dropdown-item href="/?category={{$category->slug}}&{{http_build_query(request()->except('category'))}}"
                      :active="isset($currentCategory) && $currentCategory->is($category)"
     >
         {{$category->name}}</x-dropdown-item>
