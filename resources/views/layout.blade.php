81bf66b280bfe53483c8a77a726b45673e556584
Lischen <torpille113@gmail.com>
2022-04-11 22:09:35 +0300

    lesson 49.  logout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 
 
-
-
 <title>Laravel From Scratch Blog</title>
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -10,19 +8,34 @@
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 <body style="font-family: Open Sans, sans-serif">
 <section class="px-6 py-8">
-    <nav class="md:flex md:justify-between md:items-center">
+    <nav class="md:flex md:justify-between md:items-right">
         <div>
             <a href="/">
                 <img src="images/logo.svg" alt="Laracasts Logo" width="165" height="16">
             </a>
         </div>
+        <div>
+        @guest
+            <a href="/register" class="text-xs font-bold uppercase">Register</a>
+                <a href="/login" class="text-xs font-bold uppercase">Log in</a>
+        @else
+            <div class="text-xs font-bold p-2 m-5"><p>Welcome, {{auth()->user()->name}}</p></div>
+            <form method="POST" action="/logout">
+                @csrf
+                <button type="submit"
+                        class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5"
+                >Log out</button>
+            </form>
+        @endguest
+        </div>
 
         <div class="mt-8 md:mt-0">
-            <a href="/register" class="text-xs font-bold uppercase">Register</a>
 
             <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
                 Subscribe for Updates
             </a>
+
+
         </div>
     </nav>
 
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -9,5 +10,6 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('/posts/{post:slug}', [PostController::class, 'retrieve'])->name('retrievePost');
 
-Route::get('/register', [RegisterController::class, 'create'])->name('register');
-Route::post('/register', [RegisterController::class, 'store'])->name('registerStore');
+Route::get('/register', [RegisterController::class, 'create'])->name('register')->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store'])->name('registerStore')->middleware('guest');
+Route::post('/logout', [SessionController::class, 'destroy'])->name('logout')->middleware('auth');
