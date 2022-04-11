d382bbb6a3ea15e7802501d8158aecfcd8383ee8
Lischen <torpille113@gmail.com>
2022-04-11 21:30:03 +0300

    lesson 49.  sign in registered user

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -21,7 +21,9 @@ class RegisterController extends Controller
         ]);
         $attrs['password'] = bcrypt($attrs['password']);
 
-        User::create($attrs);
+        $user = User::create($attrs);
+
+        auth()->login($user);
 
         return redirect('/')->with('success', 'Your account has been created');
     }
