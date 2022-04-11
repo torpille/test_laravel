71214fcd68d8b2ebb26269ed6b2cb25f58cf7faa
Lischen <torpille113@gmail.com>
2022-04-11 20:24:08 +0300

    lesson 47. unique validation

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
 {
@@ -14,7 +15,7 @@ class RegisterController extends Controller
     public function store() {
         $attrs = request()->validate([
             "name"=>['required', 'max:255', 'min:3'],
-            "username"=>['required', 'min:3'],
+            "username"=>['required', 'min:3', Rule::unique('users','username')],
             "email"=>['required', 'email', 'max:255'],
             "password"=>['required', 'max:255', 'min:6'],
         ]);
