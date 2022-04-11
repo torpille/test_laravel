9cef3f7bc8faf86e7bf2a1c5702e054300560b04
Lischen <torpille113@gmail.com>
2022-04-11 16:31:01 +0300

    lesson 46. use mutator

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,6 +43,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function SetPasswordAttribute($password) {
+        $this->attributes['password'] = bcrypt($password);
+    }
+
     public function posts() {
         return $this->hasMany(Post::class);
     }
