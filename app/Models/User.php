c3477431b428de0c3ef31565f504e887f944a647
Lischen <torpille113@gmail.com>
2022-04-11 16:31:56 +0300

    fix lesson 46. use mutator

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,7 +43,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function SetPasswordAttribute($password) {
+    public function setPasswordAttribute($password) {
         $this->attributes['password'] = bcrypt($password);
     }
 
