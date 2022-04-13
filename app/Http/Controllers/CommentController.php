54f96ff8fd74398e571fb40a158139b3681b5939
Lischen <torpille113@gmail.com>
2022-04-13 20:30:57 +0300

    lesson 57. throw exception

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Comment;
 use App\Models\Post;
+use Illuminate\Validation\ValidationException;
 
 class CommentController extends Controller
 {
     public function store(Post $post) {
         $attrs = request()->validate([
             'body'=>['required']]);
+        if ( ! auth()->attempt($attrs)) {
+            throw ValidationException::withMessages(['body' => 'Write something to add a comment']);
+        }
         $attrs['user_id'] = auth()->id();
         $attrs['post_id'] = $post->id;
         Comment::create($attrs);
