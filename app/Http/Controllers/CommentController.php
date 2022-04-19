e98d84aac44dab3a189379275000de9cfb985f07
Lischen <torpille113@gmail.com>
2022-04-19 09:53:34 +0300

    fix comment validation

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -11,10 +11,14 @@ class CommentController extends Controller
     public function store(Post $post) {
         $attrs = request()->validate([
             'body'=>['required']]);
-        if ( ! auth()->attempt($attrs)) {
+        if (! $attrs) {
             throw ValidationException::withMessages(['body' => 'Write something to add a comment']);
         }
-        $attrs['user_id'] = auth()->id();
+        $user_id = auth()->id();
+        if (! $user_id) {
+            throw ValidationException::withMessages(['Log in or register']);
+        }
+        $attrs['user_id'] = $user_id;
         $attrs['post_id'] = $post->id;
         Comment::create($attrs);
         return back()->with('success', 'Your comment was added');
