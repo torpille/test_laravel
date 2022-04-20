0cc4fcb0154d8372cddcb2fe3c0aee21d6e09d96
Lischen <torpille113@gmail.com>
2022-04-20 09:06:09 +0300

    fix dropdown

--- resources/views/components/dropdown-item.blade.php
+++ resources/views/components/dropdown-item.blade.php
@@ -1,12 +1,13 @@
 @props(['active' => false])
 
 @php
-    $classes = "block text-left px-3 text-sm leading-7 hover:bg-gray-300 focus:bg-gray-300";
+    $classes = "block text-left px-3 text-sm leading-7 hover:bg-gray-300 focus:bg-gray-300 relative";
         if ($active) $classes .= ' bg-gray-300';
 
 @endphp
-
-<a {{$attributes(['class'=>$classes])}}>
+<div>
+    <a {{$attributes(['class'=>$classes])}}>
     {{$slot}}
-</a>
+</a></div>
+
 
--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -6,7 +6,7 @@
     </div>
 
     {{--Dropdown links--}}
-    <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 z-50 rounded-xl overflow-auto max-h-52" style="display: none" @click.away="show=false">
+    <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 z-50 rounded-xl overflow-auto max-h-52 relative" style="display: none" @click.away="show=false">
         {{$slot}}
     </div>
 </div>
