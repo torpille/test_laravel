6288cd3548f77c83f677439bbacfcf0e9876bdf6
Lischen <torpille113@gmail.com>
2022-04-20 17:40:23 +0300

    add posts link

--- resources/views/components/setting.blade.php
+++ resources/views/components/setting.blade.php
@@ -2,26 +2,26 @@
 @extends('layout')
 
 @section('content')
-    <span class="max-w-sm" >
+    <span class="max-w-xl" >
         <h1 class="font-bold text-xl m-5 border-b-gray-300">{{$name}}</h1>
         <span class="flex">
             <aside class="w-48">
                 <ul>
                     <h4 class="font-semibold mb-6">Links</h4>
                     <li>
-                       <a href="admin/posts/create"
+                       <a href="../admin/posts/create"
                           class="{{request()->is('admin/posts/create') ? 'text-blue-500' : ''}}"
                        >Create a new post </a>
                     </li>
                     <li>
-                       <a href="admin/dashboard"
-                          class="{{request()->is('admin/dashboard') ? 'text-blue-500' : ''}}"
-                       >dashboard </a>
+                       <a href="../admin/posts"
+                          class="{{request()->is('admin/posts') ? 'text-blue-500' : ''}}"
+                       >Manage Posts </a>
                     </li>
                 </ul>
             </aside>
             <main class="flex-1">
-                <x-card class="max-w-sm" >
+                <x-card class="max-w-xxl" >
                     {{$slot}}
                 </x-card>
             </main>
